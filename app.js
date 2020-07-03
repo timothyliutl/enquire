8fb8afab8b003ea67ac01f4862b8b34ebfb4c994
Timothy Liu <18tfl1@queensu.ca>
2020-07-03 19:10:10 -0400

    mongodb and mongoose added

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { static } = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/form', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const responseSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: String,
+    tags: String
+});
+const DBresponse = mongoose.model("Response", responseSchema);
+
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 var day = new Date();
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var response = {};
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
 
-    res.render("index", {date: days[day.getDay()]});
+    res.render("index", { date: days[day.getDay()] });
 
 });
 
-app.post('/question-compositions', function(req,res){
-    
+app.post('/question-compositions', function (req, res) {
+    const question = new DBresponse({
+        title: req.body.title,
+        body: req.body.questionBody,
+        tags: req.body.tags
+    });
+    question.save();
     console.log(req.body.title);
     response = req.body;
     console.log(response);
-    res.render('preview', {title: response.title, body:response.questionBody, tags:response.tags});
+    res.render('preview', { title: response.title, body: response.questionBody, tags: response.tags });
+
 });
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server running on Port 3000");
 })
 //const in JS for things inside a variable aren't always constant, the variable itself just can't be reassigned
