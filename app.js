80b4ea603d1c4318cdfb01254fc33400b19cbac7
Timothy Liu <18tfl1@queensu.ca>
2020-07-06 02:35:25 -0400

    login page created and implemented to node js

--- app.js
+++ app.js
@@ -25,10 +25,11 @@ app.set("view engine", "ejs");
 var day = new Date();
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var response = {};
+var courses = ["APSC 112", "APSC 172", "APSC 174"];
 
 app.get('/', function (req, res) {
 
-    res.render("index", { date: days[day.getDay()] });
+    res.render("index", { date: days[day.getDay()], courselist: courses});
 
 });
 
@@ -42,8 +43,21 @@ app.post('/question-compositions', function (req, res) {
     console.log(req.body.title);
     response = req.body;
     console.log(response);
-    res.render('preview', { title: response.title, body: response.questionBody, tags: response.tags });
+    res.render('preview', { title: response.title, body: response.questionBody, tags: response.tags});
 
+});
+
+app.get("/login", function(req,res){
+res.sendFile(__dirname + "/HTML/login.html", function(err){
+    if(err){
+        console.log(err);
+    }
+})
+});
+
+app.post("/login", function(req,res){
+    console.log(req.body.username);
+    res.send("Thank You for Logging In");
 });
 
 
