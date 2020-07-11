1904fdd2b96498855dbd348d8142901f737c668b
Timothy Liu <18tfl1@queensu.ca>
2020-07-11 03:49:11 -0400

    got mathjax to work

--- public/src/form.js
+++ public/src/form.js
@@ -13,6 +13,6 @@ const submitButton = document.getElementById("examplecheckbox").addEventListener
 
 const questionBody = document.getElementById("exampleFormControlTextarea1").addEventListener("input", function(){
     //could be an issue with the "input" event listener, may not work depending on the browser
-    document.getElementById("textareapreview").value = document.getElementById("exampleFormControlTextarea1").value;
-
+    document.getElementById("textareapreview").innerHTML = document.getElementById("exampleFormControlTextarea1").value;
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 });
