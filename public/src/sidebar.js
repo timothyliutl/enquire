d39e163f215df5556baf9d199316eb1b5ff1c4b9
Timothy Liu <18tfl1@queensu.ca>
2020-07-22 01:56:51 -0400

    added ability to toggle sidebar

--- public/src/sidebar.js
+++ public/src/sidebar.js
@@ -1,3 +1,19 @@
-$(button.btn-success).on("click", function(){
-$("div.sidebar").hide();
+var temp = 0;
+
+$(document).ready(function () {
+  $("button").on("click", function () {
+    if (temp % 2 == 0) {
+        $('button.btn').html("Show Sidebar");
+      $("div.sidebar").hide();
+      $(".col-lg-6").css("margin-left","auto");
+      temp++;
+    } else {
+        $('button.btn').html("Hide Sidebar");
+        $(".col-lg-6").css("margin-left","0px");
+      $("div.sidebar").show();
+      
+      temp++;
+    }
+  });
 });
+//look into how to add animations to this later
