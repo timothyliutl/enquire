fc49278162c64f507a6b540a806de09afd5d6ebc
Timothy Liu <timothyliu@Timothys-MacBook-Pro.local>
2020-08-09 19:33:26 -0400

    added modal for question responses

--- public/src/responsemodal.js
+++ public/src/responsemodal.js
@@ -1,1 +1,15 @@
 //TODO: make js file so that text in modal matches the text in the textbox with mathjax enabled
+//TODO: add ajax so upvotes can be done without refreshing the page
+//How do we identify each button to the unique question???
+
+$('.btn').click(function(){
+$(this).toggleClass('clicked');
+});
+
+$('#responsetextarea').on('input', function(){
+    console.log("hello");
+    
+    $('#modalbodytext').text($('#responsetextarea').val());
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]); //This code is IMPORTANT, will update the mathjax everytime the keyboard is typed on
+    console.log($('#responsetextarea').html());
+});
